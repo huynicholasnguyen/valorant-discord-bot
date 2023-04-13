8ce5af3878cdfb579badc74cd2255f227c60f226
huynicholasnguyen <hnguyen62048@gmail.com>
2023-04-13 01:33:41 -0400

    Reinstalled Dependencies and attempt at test bot

--- valorantbot.py
+++ valorantbot.py
@@ -1,11 +1,21 @@
-import os
+
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
+intents = discord.Intents.default()
+intents.message_content = True
 
-d_token = os.getenv('discord_token')
-client = discord.Client()
+client = discord.Client(intents=intents)
 
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('r_OJ35S07EKXZkPfbGEuuvDaj1NBv4_Q')
