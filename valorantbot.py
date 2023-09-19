8738984b551f0b32aae60cd5f6f194da6163035e
huynicholasnguyen <hnguyen62048@gmail.com>
2023-09-18 21:57:19 -0400

    More testing

--- valorantbot.py
+++ valorantbot.py
@@ -1,6 +1,5 @@
 import discord
 import os
-import valorant
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -10,9 +9,8 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
         
-        if message.content.startswith('!hello'):
-            await message.reply('Hello!', mention_author=True)
-            await message.reply('Hello!')
+        if message.content == '!hello':
+            await message.channel.send('Hello!', mention_author=True)
             
             
 
@@ -21,7 +19,5 @@ intents.message_content = True
 intents.messages= True
 
 KEY = os.environ["RGAPI-5bf6523b-a056-446f-8193-1ba6b65e6eec"]
-valorant_client = valorant.Client(KEY, locale=None)
-skins = valorant_client.get_skins()
 client = MyClient(intents=intents)
 client.run('MTA1NzA4Mzk4Njk1NjAwNTQxNg.GNovx2.d4d76gbFkb2YQrQkb-MqNs41u4RnfMz4V9jvZM')
